5352087d8b53378bd0aaf9fa2420c30992ad7ea7
Igor Derkach <gosha753951@gmail.com>
2018-06-20 14:02:23 +0300

    Refactoring, squeeze code into one file

--- pyzkaccess.py
+++ pyzkaccess.py
@@ -1,5 +1,10 @@
 import ctypes
-from .event import ZKRealtimeEvent
+from datetime import datetime
+
+
+#
+# Enumerations
+#
 
 
 class ControlOperation:
@@ -18,6 +23,78 @@ class RelayGroup:
     lock = 1
     aux = 2
 
+
+VERIFY_MODES = {
+    '1':   'Only finger',
+    '3':   'Only password',
+    '4':   'Only card',
+    '11':  'Card and password',
+    '200': 'Others'
+}
+
+EVENT_TYPES = {
+    '0':   'Normal Punch Open',
+    '1':   'Punch during Normal Open Time Zone',
+    '2':   'First Card Normal Open (Punch Card)',
+    '3':   'Multi-Card Open (Punching Card)',
+    '4':   'Emergency Password Open',
+    '5':   'Open during Normal Open Time Zone',
+    '6':   'Linkage Event Triggered',
+    '7':   'Cancel Alarm',
+    '8':   'Remote Opening',
+    '9':   'Remote Closing',
+    '10':  'Disable Intraday Normal Open Time Zone',
+    '11':  'Enable Intraday Normal Open Time Zone',
+    '12':  'Open Auxiliary Output',
+    '13':  'Close Auxiliary Output',
+    '14':  'Press Fingerprint Open',
+    '15':  'Multi-Card Open (Press Fingerprint)',
+    '16':  'Press Fingerprint during Normal Open Time Zone',
+    '17':  'Card plus Fingerprint Open',
+    '18':  'First Card Normal Open (Press Fingerprint)',
+    '19':  'First Card Normal Open (Card plus Fingerprint)',
+    '20':  'Too Short Punch Interval',
+    '21':  'Door Inactive Time Zone (Punch Card)',
+    '22':  'Illegal Time Zone',
+    '23':  'Access Denied',
+    '24':  'Anti-Passback',
+    '25':  'Interlock',
+    '26':  'Multi-Card Authentication (Punching Card)',
+    '27':  'Unregistered Card',
+    '28':  'Opening Timeout',
+    '29':  'Card Expired',
+    '30':  'Password Error',
+    '31':  'Too Short Fingerprint Pressing Interval',
+    '32':  'Multi-Card Authentication (Press Fingerprint)',
+    '33':  'Fingerprint Expired',
+    '34':  'Unregistered Fingerprint',
+    '35':  'Door Inactive Time Zone (Press Fingerprint)',
+    '36':  'Door Inactive Time Zone (Exit Button)',
+    '37':  'Failed to Close during Normal Open Time Zone',
+    '101': 'Duress Password Open',
+    '102': 'Opened Accidentally',
+    '103': 'Duress Fingerprint Open',
+    '200': 'Door Opened Correctly',
+    '201': 'Door Closed Correctly',
+    '202': 'Exit button Open',
+    '203': 'Multi-Card Open (Card plus Fingerprint)',
+    '204': 'Normal Open Time Zone Over',
+    '205': 'Remote Normal Opening',
+    '220': 'Auxiliary Input Disconnected',
+    '221': 'Auxiliary Input Shorted',
+    '255': 'Actually that obtain door status and alarm status',
+}
+
+ENTRY_EXIT_TYPES = {
+    '0': 'Entry',
+    '1': 'Exit',
+    '2': 'None'
+}
+
+
+#
+# Device model-specific classes
+#
 
 class ZK400:
     """ZKAccess C3-400"""
@@ -46,7 +123,15 @@ class ZK100:
     groups_def = (RelayGroup.aux, RelayGroup.lock)
 
 
+#
+# Main class
+#
+
+
 class ZKAccess:
+    """
+    Main class to work on. Contains interface for working with device, implements SDK calls and holds current state
+    """
     @property
     def device_model(self):
         """Device model class. Read only"""
@@ -231,3 +316,48 @@ class ZKAccess:
             raise RuntimeError('GetRTLog failed, returned: {}'.format(str(res)))
 
         return buf.value.decode('utf-8')
+
+
+#
+# Device event
+#
+
+class ZKRealtimeEvent:
+    """
+    Represents one realtime event occured on the device
+    Since the device returns event as string we need to parse it to the structured view. This class does this.
+    """
+    __slots__ = (
+        'time',
+        'pin',
+        'card',
+        'door',
+        'event_type',
+        'entry_exit',
+        'verify_mode'
+    )
+
+    def __init__(self, s=None):
+        """
+        :param s: Optional. Event string to be parsed.
+        """
+        if s:
+            self.parse(s)
+
+    def parse(self, s):
+        """
+        Parse one event string and fills out slots
+        :param s: event string
+        :raises ValueError: event string is invalid
+        :return:
+        """
+        if s == '' or s == '\r\n':
+            raise ValueError("Empty event string")
+
+        items = s.split(',')
+        if len(items) != 7:
+            raise ValueError("Event string has not 7 comma-separated parts")
+
+        items[0] = datetime.strptime(items[0], '%Y-%m-%d %H:%M:%S')
+        for i in range(len(self.__slots__)):
+            setattr(self, self.__slots__[i], items[i])
--- tests/test_pyzkaccess.py
+++ tests/test_pyzkaccess.py
@@ -1,9 +1,9 @@
+import datetime
 from unittest.mock import patch, Mock, call
-import itertools
 
 import pytest
 
-from ..event import ZKRealtimeEvent
+from ..pyzkaccess import ZKRealtimeEvent
 
 with patch('ctypes.WinDLL', create=True):
     from ..pyzkaccess import ControlOperation, RelayGroup
@@ -285,3 +285,52 @@ class TestZKAccess:
         with patch('ctypes.create_string_buffer') as m:
             with pytest.raises(RuntimeError):
                 res = self.obj.zk_get_rt_log(buf_size)
+
+
+class TestZKRealtimeEvent:
+    @pytest.fixture(autouse=True)
+    def setUp(self):
+        self.obj = ZKRealtimeEvent()
+
+    def test_constructor_without_parse(self):
+        obj = Mock(spec=ZKRealtimeEvent)
+
+        ZKRealtimeEvent.__init__(obj)
+
+        assert obj.parse.call_count == 0
+
+    def test_constructor_with_parse(self):
+        test_data = '2017-02-09 12:37:34,0,0,1,221,2,200'
+        obj = Mock(spec=ZKRealtimeEvent)
+
+        ZKRealtimeEvent.__init__(obj, test_data)
+
+        obj.parse.assert_called_with(test_data)
+
+    def test_parse_normal(self):
+        test_data = '2017-02-09 12:37:34,0,0,1,221,2,200'
+        check_data = {
+            'time': datetime.datetime.strptime('2017-02-09 12:37:34', '%Y-%m-%d %H:%M:%S'),
+            'pin': '0',
+            'card': '0',
+            'door': '1',
+            'event_type': '221',
+            'entry_exit': '2',
+            'verify_mode': '200'
+        }
+
+        self.obj.parse(test_data)
+
+        for slot in self.obj.__slots__:
+            assert getattr(self.obj, slot) == check_data[slot]
+
+    @pytest.mark.parametrize('test_data', [
+        '2017-02-09 12:37:34,0,0,1,221,2,200\r\n2017-02-09 12:37:35,0,0,1,220,2,200\r\n',
+        '',
+        '\r\n',
+        '2017-02-09 12:37:34,0,0,1,221,2,200,5',
+        '2017-02-09 12:37:34,0,0,1,221,2'
+    ])
+    def test_parse_error(self, test_data):
+        with pytest.raises(ValueError):
+            res = self.obj.parse(test_data)
