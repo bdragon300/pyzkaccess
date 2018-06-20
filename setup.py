16a2acb7d63f53ee5335b5f6c73a57b1942692ed
Igor Derkach <gosha753951@gmail.com>
2018-06-20 15:31:33 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='pyzkaccess',
+    version='0.1',
+    packages=['', 'tests'],
+    url='https://github.com/bdragon300/pyzkaccess',
+    license='Apache 2.0',
+    author='Igor Derkach',
+    author_email='gosha753951@gmail.com',
+    description='Python interface to ZKTeco ZKAccess C3 family access control devices '
+)
