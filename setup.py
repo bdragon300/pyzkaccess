bd0f03aa85d319c038f1d022b81e149eb98b93e4
Igor Derkach <gosha753951@gmail.com>
2021-05-10 00:16:58 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pyzkaccess',
-    description='Python interface to ZKTeco ZKAccess C3-100/200/400 controllers',
+    description='Library and command-line utility '
+                'for working with ZKTeco ZKAccess C3-100/200/400 controllers',
     version='0.2',
     author='Igor Derkach',
     author_email='gosha753951@gmail.com',
@@ -19,20 +20,24 @@ setup(
     packages=setuptools.find_packages(exclude=['tests', 'docs']),
     long_description=long_description,
     long_description_content_type='text/markdown',
+    entry_points={"console_scripts": ["pyzkaccess=pyzkaccess.cli:main"]},
     classifiers=[
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Operating System :: Microsoft :: Windows',
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: Apache Software License',
         'Intended Audience :: Developers',
+        'Intended Audience :: Telecommunications Industry',
+        'Intended Audience :: Customer Service',
         'Topic :: System :: Hardware'
     ],
     # Also tox.ini
     install_requires=[
         'wrapt',
-        'fire'
+        'fire',
+        'prettytable'
     ],
 )
